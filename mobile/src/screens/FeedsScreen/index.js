ba4204876fd32be57423a1fbdf31cb720279ffff
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-05-26 16:15:51 -0400

    added react-native navigation libraries to x-code and created feeds screen scaffolding

---
+++ mobile/src/screens/FeedsScreen/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class FeedsScreen extends Component {
+ 
+    render() {
+        return (
+            <View>
+                <Text>
+                    Feeds Screen
+                </Text>
+            </View>
+            
+        );
+    }
+}
+
+export default FeedsScreen;
