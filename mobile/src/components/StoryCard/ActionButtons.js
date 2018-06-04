997cbe426c35bae4080ffcd514f68a18b5cfa3da
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-06-03 23:41:23 -0400

    created action button and nested ios-hear-outline icon

--- mobile/src/components/StoryCard/ActionButtons.js
+++ mobile/src/components/StoryCard/ActionButtons.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 class ActionButtons extends Component {
   render() {
-    return <View style={styles.root}>{}</View>;
+    return (
+      <View style={styles.root}>
+        <View style={styles.actionWrapper}>
+          <TouchableOpacity feedback="opacity" style={styles.action}>
+            <Ionicons name="ios-heart-outline" size={30} color="white" />
+          </TouchableOpacity>
+        </View>
+        <View>{}</View>
+        <View>{}</View>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   root: {
     height: 50,
-    backgroundColor: "red"
+    backgroundColor: "red",
+    paddingHorizontal: 16
+  },
+  actionWrapper: {
+    flex: 1,
+    backgroundColor: "blue"
+  },
+  action: {
+    flex: 1
   }
 });
 
