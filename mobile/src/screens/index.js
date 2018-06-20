cbb4797bbf944bed316315f01e7a7b55fc5436b0
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-06-19 21:31:03 -0400

    created login screen scaffolding

--- mobile/src/screens/index.js
+++ mobile/src/screens/index.js
@@ -2,9 +2,12 @@ import { Navigation } from "react-native-navigation";
 
 import FeedsScreen from "./FeedsScreen";
 import ExploreScreen from "./ExploreScreen";
+import LoginScreen from "./LoginScreen";
+
 import WithProvider from "../components/WithProvider";
 
 export const registerScreens = () => {
   Navigation.registerComponent("mobile.FeedsScreen", () => WithProvider(FeedsScreen));
   Navigation.registerComponent("mobile.ExploreScreen", () => WithProvider(ExploreScreen));
+  Navigation.registerComponent("mobile.LoginScreen", () => WithProvider(LoginScreen));
 };
