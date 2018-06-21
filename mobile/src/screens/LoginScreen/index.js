e1ed7c4c5b070bdb0926d510ae2b1987bf5a3a3e
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-06-20 22:25:16 -0400

    created initial input boxes

--- mobile/src/screens/LoginScreen/index.js
+++ mobile/src/screens/LoginScreen/index.js
@@ -1,19 +1,42 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar, TextInput } from "react-native";
 import { iOSColors } from "react-native-typography";
+import LinearGradient from "react-native-linear-gradient";
 
-//create app name view 06192018
+import { fonts } from "../../utils/themes/fonts";
+
+const COLORS_GRADIENTS = ["#4286f4", "#373b44"];
 
 class LoginScreen extends Component {
   state = {};
   render() {
     return (
       <View style={styles.root}>
-        <View style={styles.header}>
+        <StatusBar style="light-content" />
+        <LinearGradient
+          //left to right
+          start={{
+            x: 0.0,
+            y: 0.0
+          }}
+          end={{
+            x: 1.0,
+            y: 1.0
+          }}
+          colors={COLORS_GRADIENTS}
+          style={styles.header}
+        >
           <Text style={styles.appName}>Saga Beta</Text>
-        </View>
+        </LinearGradient>
         <View style={styles.content}>
-          <Text>Hello</Text>
+          <View style={styles.section}>
+            <View style={styles.inputWrapper}>
+              <TextInput style={styles.input} placeholder="Email" />
+            </View>
+            <View style={styles.inputWrapper}>
+              <TextInput style={styles.input} placeholder="Password" />
+            </View>
+          </View>
         </View>
       </View>
     );
@@ -35,12 +58,30 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    backgroundColor: "blue",
     alignSelf: "stretch"
   },
   appName: {
     color: iOSColors.white,
-    fontSize: 50
+    fontSize: 50,
+    fontFamily: fonts.sunflower
+  },
+  section: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inputWrapper: {
+    height: 45,
+    width: "90%",
+    backgroundColor: "#FAF9F9",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#E4E4E4",
+    marginBottom: 10,
+    padding: 10
+  },
+  input: {
+    flex: 1
   }
 });
 
