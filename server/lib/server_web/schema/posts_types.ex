a7a4a4e1f88b57bf66c463e6707a113869143302
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-07-03 14:26:13 -0400

    created view like resolver and added to posts types

--- server/lib/server_web/schema/posts_types.ex
+++ server/lib/server_web/schema/posts_types.ex
@@ -1,10 +1,16 @@
 defmodule ServerWeb.Schema.PostsTypes do
   use Absinthe.Schema.Notation
 
+  alias ServerWeb.Resolvers
+
   object :photo do
     field(:id, non_null(:id))
     field(:image_url, non_null(:string))
     field(:caption, :string)
+
+    field(:viewer_like, non_null(:boolean)) do
+      resolve(&Resolvers.Reactions.viewer_like_photo/3)
+    end
 
     field(:inserted_at, non_null(:string))
     field(:updated_at, non_null(:string))
