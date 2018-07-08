8715eb442df94610fd0ab8aea60c84e28883d598
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-07-08 15:27:22 -0400

    created scaffolding for create photo screen and added to index

---
+++ mobile/src/screens/CreatePhotoScreen/index.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+class CreatePhotoScreen extends PureComponent {
+  state = {};
+  render() {
+    return (
+      <View>
+        <Text>Photo Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default CreatePhotoScreen;
