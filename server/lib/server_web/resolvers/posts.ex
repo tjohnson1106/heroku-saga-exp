cfd7ec6d335513fcb52c8cba2b709324c0787d79
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-07-07 20:31:27 -0400

    create create photo function to posts

--- server/lib/server_web/resolvers/posts.ex
+++ server/lib/server_web/resolvers/posts.ex
@@ -1,13 +1,23 @@
 defmodule ServerWeb.Resolvers.Posts do
+  alias Server.Posts
+
   def photos(_, _, _) do
-    {:ok, Server.Posts.list_photos()}
+    {:ok, Posts.list_photos()}
   end
 
   def photo(_, %{id: id}, _) do
-    {:ok, Server.Posts.get_photo!(id)}
+    {:ok, Posts.get_photo!(id)}
   end
 
   def presign_url(_, _, _) do
-    {:ok, Server.Posts.get_presign_url()}
+    {:ok, Posts.get_presign_url()}
+  end
+
+  def create_photo(_, args, %{context: %{current_user: current_user}}) do
+    args = Map.merge(args, %{user_id: current_user.id})
+
+    with {:ok, photo} <- Posts.create_photo(args) do
+      {:ok, photo}
+    end
   end
 end
