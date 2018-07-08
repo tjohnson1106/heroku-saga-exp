efab261c7e9a3815b692b5911cafed7290c1974e
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-07-07 20:21:56 -0400

    created function to add user id to photo via random me api

--- server/lib/server/accounts/user.ex
+++ server/lib/server/accounts/user.ex
@@ -18,10 +18,13 @@ defmodule Server.Accounts.User do
   end
 
   @doc false
+
+  #   :first_name, :last_name removed from validate
+
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:username, :avatar, :facebook_id, :email, :first_name, :last_name])
-    |> validate_required([:avatar, :email, :facebook_id, :first_name, :last_name])
+    |> validate_required([:avatar, :email, :facebook_id])
     |> unique_constraint(:email)
     |> unique_constraint(:facebook_id)
   end
