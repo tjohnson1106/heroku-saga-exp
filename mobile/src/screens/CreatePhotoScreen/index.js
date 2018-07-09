a5711145fd3e07e279058fc08529b852a6e076aa
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-07-08 21:28:47 -0400

    created function to get initial camera roll image state

--- mobile/src/screens/CreatePhotoScreen/index.js
+++ mobile/src/screens/CreatePhotoScreen/index.js
@@ -1,8 +1,36 @@
 import React, { PureComponent } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, CameraRoll, Image } from "react-native";
+
+const MAX_PHOTOS = 20;
 
 class CreatePhotoScreen extends PureComponent {
-  state = {};
+  state = {
+    images: []
+  };
+
+  componentDidMount() {
+    this._getPhotos;
+  }
+
+  _getPhotos = async () => {
+    const res = await CameraRoll.getPhotos({
+      first: MAX_PHOTOS,
+      after
+    });
+
+    this.setState({
+      images: [...this.state.images, res.edges]
+    });
+  };
+
+  _renderItem = ({ item }) => {
+    return (
+      <View style={styles.imageWrapper}>
+        <Image source={{ uri: item.node.image.uri }} style={styles._image} />
+      </View>
+    );
+  };
+
   render() {
     return (
       <View>
@@ -11,5 +39,15 @@ class CreatePhotoScreen extends PureComponent {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  imageWrapper: {
+    width: 100,
+    height: 100
+  },
+  image: {
+    flex: 1
+  }
+});
 
 export default CreatePhotoScreen;
